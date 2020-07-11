f1c82cc6a1c6f10056697eea4f3811d21dd0cb99
Angel Gutierrez <angel50710@hotmail.com>
2020-07-11 17:33:54 -0500

    Creando POST, creacion de usuarios"

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let URLDB;
 if (process.env.NODE_ENV == 'dev') {
-    URLDB='mongodb+srv://4Ny3L0:AgM0nG0.06.M@cluster0.aaqdn.mongodb.net/cafe';
+    URLDB='mongodb://localhost:27017/cafe';
 }else{
     URLDB='mongodb+srv://4Ny3L0:AgM0nG0.06.M@cluster0.aaqdn.mongodb.net/cafe'
 }
